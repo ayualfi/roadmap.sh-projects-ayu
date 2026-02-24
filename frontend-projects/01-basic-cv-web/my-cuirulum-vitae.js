8f529aebbf17db330a5d1b643ee5e49c6f210f41
Ayu Alfi <ayu425537@gmail.com>
2026-02-24 13:33:56 +0700

    belajar while

--- frontend-projects/01-basic-cv-web/my-cuirulum-vitae.js
+++ frontend-projects/01-basic-cv-web/my-cuirulum-vitae.js
@@ -1,3 +1,23 @@
-var x=10
-console.log("Hello World")
-console.log("Nilai variabel x = " + x)
+// var konfir = confirm("Kamu yakin ??");
+// ulang = true;
+
+// while (ulang){
+//     var nama = prompt("Bagaimana aku memanggilmu?")
+//     alert("Halo"+nama)
+
+//     ulang=confirm("Coba lagi?")
+// }
+
+// alert("Terimakasih "+nama)
+
+var angka = 1
+while(angka<=10){
+    console.log('Hallo')
+    angka++
+}
+
+ulang = true
+while (ulang){
+    prompt("Kamu suka makan apa aja?")
+    ulang=confirm("Ada lagi?")
+}
